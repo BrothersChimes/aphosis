225f3a0e126d6ac2561d6b624bbd414d3feffa55
Liam Chimes <stunbrick@gmail.com>
2025-01-25 01:18:55 +0200

    sprinting

--- player.gd
+++ player.gd
@@ -2,11 +2,16 @@ extends RigidBody2D
 
 const rotation_speed = 9000
 const max_rotation_speed = 5.0
-const max_speed = 240.0
+const max_speed = 500.0
 const thrust = 12000
-const forward_friction = 6.0
+const forward_friction = 4.0
 const sideways_friction = 120.0
 const max_friction = 60000.0
+
+const sprint_rotation_speed = 12000
+const sprint_max_rotation_speed = 5.0
+const sprint_max_speed = 500.0
+const sprint_thrust = 60000
 
 var current_angular_velocity: float = 0.0  # Custom angular velocity
 
@@ -21,10 +26,15 @@ func _physics_process(delta: float) -> void:
 
 	var sideways_speed_ratio = sideways_velocity.length() / max_speed  # Calculate sideways speed ratio
 
-	var rotation_speed_ratio = abs(current_angular_velocity) / max_rotation_speed
 
 	# Calculate effective rotation speed based on current rotation speed
-	var effective_rotation_speed = lerp(rotation_speed, 0, rotation_speed_ratio)
+	var effective_rotation_speed
+	if Input.is_action_pressed('sprint'):
+		var rotation_speed_ratio = abs(current_angular_velocity) / sprint_max_rotation_speed
+		effective_rotation_speed = lerp(sprint_rotation_speed, 0, rotation_speed_ratio)
+	else:
+		var rotation_speed_ratio = abs(current_angular_velocity) / max_rotation_speed
+		effective_rotation_speed = lerp(rotation_speed, 0, rotation_speed_ratio)
 
 	if Input.is_action_pressed('right_button'):
 		apply_torque(effective_rotation_speed * delta)
@@ -33,8 +43,13 @@ func _physics_process(delta: float) -> void:
 		
 
 	if (Input.is_action_pressed('up_button')):
-		var speed_ratio = current_velocity.length() / max_speed
-		var applied_thrust = lerp(thrust, 0, speed_ratio)  # Decrease thrust as speed approaches max_speed
+		var applied_thrust
+		if Input.is_action_pressed('sprint'):
+			var speed_ratio = current_velocity.length() / sprint_max_speed
+			applied_thrust = lerp(sprint_thrust, 0, speed_ratio)  # Decrease thrust as speed approaches max_speed
+		else:
+			var speed_ratio = current_velocity.length() / max_speed
+			applied_thrust = lerp(thrust, 0, speed_ratio)  # Decrease thrust as speed approaches max_speed
 		apply_central_force(transform.y * -applied_thrust * delta)
 	
 	# Apply forward and sideways friction
